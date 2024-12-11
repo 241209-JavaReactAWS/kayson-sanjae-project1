2a4699cb26cb9ad3f20b2c2a96da2ad05314156e
Sanjae Chin <sanjae.chin@gmail.com>
2024-12-11 13:29:38 -0500

    Set up Pokemon class

--- Pokemon/src/main/java/com/revature/models/Pokemon.java
+++ Pokemon/src/main/java/com/revature/models/Pokemon.java
@@ -1,5 +1,50 @@
 package com.revature.models;
 
 public class Pokemon{
+    private int pokemonId;
+    private String name;
+    private PokemonType type1;
+    private PokemonType type2;
 
+    public Pokemon(){
+
+    }
+
+    public Pokemon(String name, PokemonType type1, PokemonType type2) {
+        this.name = name;
+        this.type1 = type1;
+        this.type2 = type2;
+    }
+
+    public int getPokemonId() {
+        return pokemonId;
+    }
+
+    public void setPokemonId(int pokemonId) {
+        this.pokemonId = pokemonId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PokemonType getType1() {
+        return type1;
+    }
+
+    public void setType1(PokemonType type1) {
+        this.type1 = type1;
+    }
+
+    public PokemonType getType2() {
+        return type2;
+    }
+
+    public void setType2(PokemonType type2) {
+        this.type2 = type2;
+    }
 }
---
+++ Pokemon/src/main/java/com/revature/models/PokemonType.java
@@ -0,0 +1,23 @@
+package com.revature.models;
+
+public enum PokemonType {
+    NORMAL,
+    FIRE,
+    WATER,
+    ELECTRIC,
+    GRASS,
+    ICE,
+    FIGHTING,
+    POISON,
+    GROUND,
+    FLYING,
+    PSYCHIC,
+    BUG,
+    ROCK,
+    GHOST,
+    DRAGON,
+    DARK,
+    STEEL,
+    FAIRY,
+    STELLAR
+}
