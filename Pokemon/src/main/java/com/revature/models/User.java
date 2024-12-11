4513a66e3346818620ceb6a6a165f6423b93ba99
Sanjae Chin <sanjae.chin@gmail.com>
2024-12-11 13:19:59 -0500

    Drafted User Class

--- Pokemon/src/main/java/com/revature/models/User.java
+++ Pokemon/src/main/java/com/revature/models/User.java
@@ -1,8 +1,56 @@
 package com.revature.models;
 
+import jakarta.annotation.Generated;
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+    private String username;
+    private String password;
+    private boolean isAdmin;
+
+    public User(){
+    }
+
+    public User(String username, String password, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
 }
