a2b8ea6f61fb46b81945cd5e8cdcace65d587a99
Kayson <klkxxy@gmail.com>
2024-12-16 17:20:47 -0500

    add PokemonService

--- Pokemon/src/main/java/com/revature/daos/PokemonDao.java
+++ Pokemon/src/main/java/com/revature/daos/PokemonDao.java
@@ -4,7 +4,10 @@ import com.revature.models.Pokemon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PokemonDao extends JpaRepository<Pokemon, Integer> {
-    
+
+    Optional<Pokemon> findByName(String name);
 }
---
+++ Pokemon/src/main/java/com/revature/services/PokemonService.java
@@ -0,0 +1,36 @@
+package com.revature.services;
+
+import com.revature.daos.PokemonDao;
+import com.revature.models.Pokemon;
+import com.revature.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PokemonService {
+    private final PokemonDao pokemonDao;
+    @Autowired
+    public PokemonService(PokemonDao pokemonDao){
+        this.pokemonDao = pokemonDao;
+    }
+
+
+    public Pokemon savePokemon(Pokemon pokemon){
+        return pokemonDao.save(pokemon);
+    }
+
+    public Optional<Pokemon> findPokemonByName(String name){
+        return pokemonDao.findByName(name);
+    }
+
+    public Optional<Pokemon> findUserById(int id){
+        return pokemonDao.findById(id);
+    }
+
+    public List<Pokemon> allPokemons(){
+        return pokemonDao.findAll();
+    }
+}
