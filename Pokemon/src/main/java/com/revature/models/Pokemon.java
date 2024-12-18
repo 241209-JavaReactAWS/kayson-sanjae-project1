6b9cb58de243a9f768bc04739d6722e7dd28af51
Kayson <klkxxy@gmail.com>
2024-12-18 16:11:52 -0500

    Complete Pokemon controller.

--- Pokemon/src/main/java/com/revature/models/Pokemon.java
+++ Pokemon/src/main/java/com/revature/models/Pokemon.java
@@ -6,7 +6,9 @@ import jakarta.persistence.*;
 @Table(name="pokemons")
 public class Pokemon{
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int pokemonId;
+    @Column(unique = true)
     private String name;
     @Enumerated(EnumType.STRING)
     private PokemonType type1;
