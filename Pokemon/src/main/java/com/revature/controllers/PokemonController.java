1c133516ca4d97d2c415a101371b3de22754d06a
Kayson <klkxxy@gmail.com>
2024-12-18 13:41:33 -0500

    add pokemon and user controllers

---
+++ Pokemon/src/main/java/com/revature/controllers/PokemonController.java
@@ -0,0 +1,14 @@
+package com.revature.controllers;
+
+import com.revature.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+}
