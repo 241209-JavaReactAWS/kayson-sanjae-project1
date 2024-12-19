86b6587bd263b3b0bb4df9857c3dcbffa16c85c4
Kayson <klkxxy@gmail.com>
2024-12-19 11:31:05 -0500

    add crossOrigin

--- Pokemon/src/main/java/com/revature/controllers/UserController.java
+++ Pokemon/src/main/java/com/revature/controllers/UserController.java
@@ -2,9 +2,11 @@ package com.revature.controllers;
 
 import com.revature.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class UserController {
     private final UserService userService;
     @Autowired
