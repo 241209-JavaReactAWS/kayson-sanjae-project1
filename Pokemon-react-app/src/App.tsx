06d31642dd13685554b0f27cb0f0ca821a7ddbff
Sanjae Chin <sanjae.chin@gmail.com>
2024-12-19 16:41:56 -0500

    final commit for today

--- Pokemon-react-app/src/App.tsx
+++ Pokemon-react-app/src/App.tsx
@@ -7,8 +7,10 @@ import Pokemon_m from './components/Pokemon_m/Pokemon_m'
 import Login from './components/login/login'
 import { createContext, useEffect, useState } from 'react'
 import axios from 'axios'
+import Collection from "./components/collection/Collection";
 
 export interface AuthContextType{
+  userId : number,
   username: string,
   setUsername: (username: string) => void,
   role: "unauthenticated" | "USER" | "ADMIN",
@@ -19,6 +21,7 @@ export const authContext = createContext<AuthContextType | null>(null);
 
 
 function App() {
+  const [userId, setUserId] = useState<number>(-1);
   const [username, setUsername] = useState<string>('')
   const [role, setRole] = useState<"unauthenticated" | "USER" | "ADMIN">('unauthenticated')
   
@@ -26,11 +29,13 @@ function App() {
   useEffect(()=>{
     axios.get<User>('http://localhost:8080/users', {withCredentials: true})
     .then((res) => {
+      setUserId(res.data.userId)
       setUsername(res.data.username)
       setRole(res.data.role as "USER" | "ADMIN")
     })
     .catch((err) => {
       console.log("Guest user: " + err)
+      setUserId(-1)
       setUsername('')
       setRole('unauthenticated')
     })
@@ -39,6 +44,7 @@ function App() {
   return (
     <authContext.Provider value={
       {
+        userId,
         username,
         setUsername,
         role,
@@ -54,7 +60,8 @@ function App() {
         <Route path='/logout' element />
         <Route path='/pokemon-management' element={<Pokemon_m/>} />
         <Route path='/user-management' element />
-        
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/shop' element={<Collection/>}/>
       </Routes>
 
       </BrowserRouter>
